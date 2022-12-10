018ab4a4be6e9e688714d03d15267ea433f67929
Mattia Manzati <mattia.manzati@gmail.com>
2022-12-10 23:00:01 +0100

    improve no-curry-arrow with multiple args support

--- src/rules/no-curry-arrow.ts
+++ src/rules/no-curry-arrow.ts
@@ -4,37 +4,33 @@ import { AST_NODE_TYPES } from "@typescript-eslint/utils"
 export default utils.createRule({
   create(context) {
     return {
-      // given (_) => T.log(_)
+      // given (_) => T.log(_) or () => T.unit()
       ArrowFunctionExpression(node) {
-        // has only 1 parameter
-        if (node.params.length === 1) {
-          const parameter = node.params[0]
-          // (_) should be an identifier
-          if (parameter.type === AST_NODE_TYPES.Identifier) {
-            const callExpression = node.body
-            // T.log(_) is a call expression
-            if (callExpression.type === AST_NODE_TYPES.CallExpression) {
-              const args = callExpression.arguments
-              // T.log(_) has only one argument
-              if (args.length === 1) {
-                const callArg = args[0]
-                // T.log(_) argument should be an identifier
-                if (callArg.type === AST_NODE_TYPES.Identifier) {
-                  // and _ should be the same as the argument
-                  if (callArg.name === parameter.name) {
-                    context.report({
-                      messageId: "noCurryArrow",
-                      node,
-                      fix: (fixer) => [
-                        // remove part before call expression's expression (arrow declaration)
-                        fixer.removeRange([node.range[0], callExpression.callee.range[0]]),
-                        // remove part after expression (parenthesis and arguments)
-                        fixer.removeRange([callExpression.callee.range[1], node.range[1]])
-                      ]
-                    })
-                  }
-                }
-              }
+        const callExpression = node.body
+        const params = node.params
+        // T.log(_) is a call expression
+        if (callExpression.type === AST_NODE_TYPES.CallExpression) {
+          const args = callExpression.arguments
+          // T.log(_) has same number of arguments as the arrow
+          if (args.length === params.length) {
+            // parameters and arguments are both identifiers, with same name
+            if (
+              args.every((arg, idx) => {
+                const param = params[idx]
+                return arg.type === AST_NODE_TYPES.Identifier && param.type === AST_NODE_TYPES.Identifier &&
+                  arg.name === param.name
+              })
+            ) {
+              context.report({
+                messageId: "noCurryArrow",
+                node,
+                fix: (fixer) => [
+                  // remove part before call expression's expression (arrow declaration)
+                  fixer.removeRange([node.range[0], callExpression.callee.range[0]]),
+                  // remove part after expression (parenthesis and arguments)
+                  fixer.removeRange([callExpression.callee.range[1], node.range[1]])
+                ]
+              })
             }
           }
         }
