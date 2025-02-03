42f9579998c3a91374d85caee7cfecfa2eea5be5
Mattia Manzati <mattia.manzati@gmail.com>
2025-02-03 13:02:21 +0100

    refactor esm circularity

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,17 +1,16 @@
 import { dprint } from "@effect/eslint-plugin/rules/dprint"
 
+export const meta = {
+  name: "@effect/eslint-plugin",
+}
+
+export const rules = {
+  dprint,
+}
+
 // NOTE: unfortunately plugins needs a self-reference inside configs,
 // so in this file we only stub out known configs
 // but they actually get injected in their respective file
-
-export default {
-  meta: {
-    name: "@effect/eslint-plugin",
-  },
-  rules: {
-    dprint,
-  },
-  configs: {
-    recommended: [] as Array<any>,
-  },
+export const configs = {
+  recommended: [] as Array<any>,
 }
