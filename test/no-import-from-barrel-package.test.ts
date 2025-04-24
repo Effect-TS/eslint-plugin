b44ae49d188a1419aedda722093a6cfeffe77109
Mattia Manzati <mattia.manzati@gmail.com>
2025-04-24 10:11:07 +0200

    Add rule for disallowing direct barrel imports (#30)

---
+++ test/no-import-from-barrel-package.test.ts
@@ -0,0 +1,45 @@
+import type { Options } from "@effect/eslint-plugin/rules/no-import-from-barrel-package"
+import { noImportFromBarrelPackage as rule } from "@effect/eslint-plugin/rules/no-import-from-barrel-package"
+import { ruleTester } from "@effect/eslint-plugin/test/utils/index"
+
+const options: [Options] = [{ packageNames: ["effect"] }]
+
+ruleTester.run("dprint", rule, {
+  valid: [
+    {
+      code: `import * as T from "effect/Effect"`,
+      options,
+    },
+    {
+      code: `import { Effect as Eff} from "effect/Effect"
+`,
+      options,
+    },
+    {
+      code: `import Effect from "effect/Effect"`,
+      options,
+    },
+    {
+      code: `import {type Effect } from "effect";`,
+      options,
+    },
+    {
+      code: `import {test} from "lodash";`,
+      options,
+    },
+  ],
+  invalid: [
+    {
+      code: `import { Effect } from "effect"`,
+      options,
+      errors: [{ line: 1, messageId: "replaceImport" }],
+      output: `import * as Effect from "effect/Effect"`,
+    },
+    {
+      code: `import { Effect as Eff } from "effect"`,
+      options,
+      errors: [{ line: 1, messageId: "replaceImport" }],
+      output: `import * as Eff from "effect/Effect"`,
+    },
+  ],
+})
